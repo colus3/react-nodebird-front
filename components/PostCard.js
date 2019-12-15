4414f4a1de3dd098146f4bf0b7820684f012825f
colus3 <colus4@gmail.com>
2019-12-16 00:34:08 +0900

    6-2. next와 express 연결하기 ...까지

--- components/PostCard.js
+++ components/PostCard.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 import {
   Avatar, Button, Card, Icon, Form, Input, List, Comment,
 } from 'antd';
+import Link from 'next/link';
 import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
 import { ADD_COMMENT_REQUEST } from '../reducers/post';
@@ -55,7 +56,18 @@ const PostCard = ({ post }) => {
         <Card.Meta
           avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
           title={post.User.nickname}
-          description={post.content}
+          description={(
+            <div>
+              {post.content.split(/(#[^\s]+)/g).map((v) => {
+                if (v.match(/#[^\s]+/)) {
+                  return (
+                    <Link href="/hashtag" key={v}><a>{v}</a></Link>
+                  );
+                }
+                return v;
+              })}
+            </div>
+          )} // a tag x -> Link
         />
       </Card>
       {commentFormOpened && (
